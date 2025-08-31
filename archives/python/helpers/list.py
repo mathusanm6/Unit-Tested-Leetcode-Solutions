b3505e60e0dc93878292be772e5ba64086a5a9d1
mathusanm6 <hi@mathusan.me>
2025-08-31 10:03:28 +0200

    cleanup: remove archives/ directory

--- archives/python/helpers/list.py
+++
@@ -1,9 +0,0 @@
-def equal_ignore_order(a, b):
-    """Use only when elements are neither hashable or sortable!"""
-    unmatched = list(b)
-    for element in a:
-        try:
-            unmatched.remove(element)
-        except ValueError:
-            return False
-    return not unmatched
